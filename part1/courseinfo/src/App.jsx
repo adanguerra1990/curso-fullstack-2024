a2d84c003af6dd82b0a29fedc849a02ebe9da63d
adanguerra1990 <aj90guerra@gmail.com>
2024-01-17 16:45:11 -0400

    Ejercicio 1.2: información del curso, paso 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,16 +7,28 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  console.log('Content', props)
+const Part = (props) => {
+  console.log('Part', props)
+
   return (
     <p> {props.part} {props.exercises}</p>
   )
 }
 
+const Content = (props) => {
+  console.log('Content', props)
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
 const Total = (props) => {
   console.log('Total', props)
-  
+
   return (
     <p>Number of exercises {props.total} </p>
   )
@@ -34,9 +46,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
