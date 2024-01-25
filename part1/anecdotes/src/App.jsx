9b7e0352805b19f68245e57dca3916728f71a13f
adanguerra1990 <aj90guerra@gmail.com>
2024-01-25 12:00:28 -0400

    Ejercicio 1.13 1.14*: anecdotes, paso 2 y 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+const Anecdotes = ({anecdotes}) => <h2>{anecdotes}</h2>
+
+const Votes = ({votes, text}) => <p>{text}: {votes}</p> 
+
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const MostVotedAnecdote = ({mostVotedAnecdote}) => <h2>{mostVotedAnecdote}</h2>
 
 function App() {
   const anecdotes = [
@@ -15,18 +21,33 @@ function App() {
     'The only way to go fast, is to go well.'
   ]
   const [select, setSelect] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  console.log('votes..', votes)
 
-  const nextAnecdotes = () => {
+  const handlNextAnecdotes = () => {
     const numOfAnecdotes = anecdotes.length ;    
     const randomAnecdote = Math.floor(Math.random() * numOfAnecdotes);
     console.log('click....', randomAnecdote)
     setSelect(randomAnecdote)
   }
 
+  const handleVote = () => {
+    const votesCopy = [... votes]
+    votesCopy[select] += 1
+    setVotes(votesCopy)
+  }
+
+  const findMostVotedIndex = () => {
+    return votes.reduce((maxIndex, value, index, arr) => value > arr[maxIndex] ? index : maxIndex, 0)
+  }
+
   return (
     <div>
-      <p>{anecdotes[select]}</p>
-      <Button handleClick={nextAnecdotes} text={'Next anecdotes'} />
+      <Anecdotes anecdotes={anecdotes[select]} />
+      <Votes text={'Has voted'} votes={votes[select]}/>
+      <Button handleClick={handleVote} text={'Vote'} />
+      <Button handleClick={handlNextAnecdotes} text={'Next anecdotes'} />
+      <MostVotedAnecdote mostVotedAnecdote={anecdotes[findMostVotedIndex()]} />
     </div>
   )
 }
